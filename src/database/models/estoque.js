9315e704fac5da130da22b68e2bff96d3c06d7a5
Mauboa <mba097@gmail.com>
2022-08-12 10:02:45 -0300

    fix: remove desconto to estoque

--- src/database/models/estoque.js
+++ src/database/models/estoque.js
@@ -29,11 +29,6 @@ class estoque extends Sequelize.Model {
       allowNull: true,
       defaultValue: 0.00
     },
-    desconto: {
-      type: DataTypes.DOUBLE(7,2),
-      allowNull: true,
-      defaultValue: 0.00
-    },
     status_venda: {
       type: DataTypes.BOOLEAN,
       allowNull: true,
